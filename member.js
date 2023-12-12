fc8d40b87bd677fa843a9e06d6608b78c6f8c61f
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-12-12 02:15:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/member/member.html',
+    controller: 'MemberController',
+    controllerAs: 'memberCtrl',
+    bindToController: true
+  };
+}
